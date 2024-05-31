4fda954bd431b01e54a1ed6cb86e66f874fdf53c
wrlakshan <rashmika.lakshan@ebuilder.com>
2024-05-31 14:41:17 +0530

    change name

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <h1>{user?.signInDetails?.loginId} todos</h1>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
